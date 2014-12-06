8f0f6c6859c64a62e99e2f857e098cbebd8bb131
ilyabasiuk <ilyabasiuk@gmail.com>
2014-12-05 22:40:24 -0500

    peerConnection created

--- js/webRtcHelper.js
+++ js/webRtcHelper.js
@@ -1,21 +1,34 @@
 define(
     'webRtcHelper',
     function( ){
-        navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+        navigator.getUserMedia =  navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+        var peerConnection = window.mozRTCPeerConnection || window.webkitRTCPeerConnection,
+            iceCandidate = window.mozRTCIceCandidate || window.RTCIceCandidate,
+            sessionDescription = window.mozRTCSessionDescription || window.RTCSessionDescription,
+            pc,
+            createPc = function(stream) {
+                pc = new peerConnection(null);
+                pc.addStream(stream);
+                console.log("peer connection created",pc);
+            };
 
         return {
             captureStream: function(config, onSuccess, onError ) {
                  var defaultOptions = {
                     audio: true,
-                    video: true
+                    video: true,
+                    createPc: true
                  },
                  options = defaultOptions,
                  callback = onSuccess? onSuccess : function() { console.log("we've got the stream")},
                  errorCallback = onError? onError : function(e) { alert("couldn't get the stream"); console.log(e)};
-                         // Step 1. getUserMedia
+
                  navigator.getUserMedia(
                    { audio: options.audio, video: options.video },
-                    callback,
+                    function(stream) {
+                      callback && callback(stream);
+                      options.createPc && createPc(stream);
+                    },
                     errorCallback
                  );
             }
