7b36363dedc1bd267889468d857d64a50ed88f6e
ilyabasiuk <ilyabasiuk@gmail.com>
2014-12-07 19:35:05 -0500

    adde stream data analyser to smooth the results

--- js/appReceiver.js
+++ js/appReceiver.js
@@ -1,6 +1,21 @@
-require(["webRtcHelper", "motionDetector", "alerter", "visualizer"],
+require(["webRtcHelper", "motionDetector", "alerter", "visualizer",
+    "https://rawgit.com/ilyabasiuk/streamDataAnalizer/master/streamDataAnalyzer.js"],
     function(webRtcHelper, motionDetector, createAlerter, createVisualizer){
-       var videoElem = document.createElement("video");
+       var videoElem = document.createElement("video"),
+           analyzer = streamDataAnalizer({
+                expiredTime: 1000,
+                defaultValue: 0
+              }),
+           createVisualElem = function() {
+              var el = document.createElement("canvas");
+
+              el.setAttribute("width", 20);
+              el.setAttribute("heigth", 250);
+              document.body.appendChild(el);
+
+              return el;
+           };
+
        document.body.appendChild(videoElem);
        videoElem.setAttribute("autoplay", "");
        videoElem.setAttribute("muted", "");
@@ -9,20 +24,22 @@ require(["webRtcHelper", "motionDetector", "alerter", "visualizer"],
        webRtcHelper.onStreamReceived(function(stream){
           var alertElem = document.createElement("div"),
               alerter = createAlerter(alertElem,1.5),       // if more than 1.5 % of screen changed will conseder that moving detected
-              visElem = document.createElement("canvas"),
-              viser = createVisualizer(visElem);
-              
-          visElem.setAttribute("width", 20);
-          visElem.setAttribute("heigth", 250);
-          document.body.appendChild(visElem);
+              viser = createVisualizer(createVisualElem()),
+              viserAn = createVisualizer(createVisualElem());
+
+
 
           alertElem.classList.add("result");
           document.body.appendChild(alertElem);
           videoElem.src = URL.createObjectURL(stream);
 
-          motionDetector.init(stream, videoElem, {callback: function(k) {
-              alerter.put(k);
-              viser.set(k);
+          motionDetector.init(stream, videoElem, {callback: function(percent) {
+              var filteredValue;
+              analyzer.add(percent)
+              viser.set(percent);
+              filteredValue = analyzer.get();
+              viserAn.set(filteredValue);
+              alerter.put(filteredValue);
           }});
        });
     }
